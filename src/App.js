99eb4cafed2e7fae2c01b29eba493c414d6411bb
Henry Hung <apple@appledeMacBook-Pro.local>
2022-11-19 21:06:26 +0800

    initalize

---
+++ src/App.js
@@ -0,0 +1,202 @@
+// 0. Install fingerpose npm install fingerpose
+// 1. Add Use State
+// 2. Import emojis and finger pose import * as fp from "fingerpose";
+// 3. Setup hook and emoji object
+// 4. Update detect function for gesture handling
+// 5. Add emoji display to the screen
+
+///////// NEW STUFF ADDED USE STATE
+import React, { useRef, useState, useEffect } from "react";
+///////// NEW STUFF ADDED USE STATE
+
+// import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import * as handPoseDetection from "@tensorflow-models/hand-pose-detection"
+import Webcam from "react-webcam";
+import "./App.css";
+import { drawHand } from "./utilities";
+
+///////// NEW STUFF IMPORTS
+import * as fp from "fingerpose";
+import * as fpg from "fingerpose-gestures";
+import victory from "./victory.png";
+import splayed from "./splayed.png";
+import rock from './rock.png'
+///////// NEW STUFF IMPORT
+
+
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+
+  ///////// NEW STUFF ADDED STATE HOOK
+  const [emoji, setEmoji] = useState(null);
+  const images = { finger_splayed: splayed, victory: victory, fist: rock };
+  ///////// NEW STUFF ADDED STATE HOOK
+
+  const runHandpose = async () => {
+    const model = handPoseDetection.SupportedModels.MediaPipeHands;
+    const detectorConfig = {
+      runtime: 'mediapipe', // or 'tfjs',
+      solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/hands',
+      modelType: 'full'
+    }
+    const detector = await handPoseDetection.createDetector(model, detectorConfig);
+    // const net = await handpose.load();
+    console.log("Handpose model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(detector);
+    });
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const hand = await net.estimateHands(video);
+      if (hand.length > 0) {
+        
+        //使用keypoint3D
+        const keypointsAry = Object.values(hand[0].keypoints3D)
+
+        for (let handSide in keypointsAry) {
+          keypointsAry[handSide] = Object.values(keypointsAry[handSide])
+        }
+
+
+        const GE = new fp.GestureEstimator([
+          //引入 fingerposeGesture 套件，
+          //剪刀
+          fpg.Gestures.victoryGesture,
+          //布
+          fpg.Gestures.fingerSplayedGesture,
+          //石頭
+          fpg.Gestures.fistGesture
+        ]);
+        const gesture = await GE.estimate(keypointsAry, 4);
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+          // console.log(gesture.gestures);
+
+          const confidence = gesture.gestures.map(
+            (prediction) => prediction.confidence
+          );
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          );
+          // console.log(gesture.gestures[maxConfidence].name);
+          console.log(gesture.gestures)
+          setEmoji(gesture.gestures[maxConfidence].name);
+        }
+      }
+      ///////// NEW STUFF ADDED GESTURE HANDLING
+
+      // if (hand.length > 0) {
+      //   const GE = new fp.GestureEstimator([
+      //     fp.Gestures.VictoryGesture,
+      //     fp.Gestures.ThumbsUpGesture,
+      //   ]);
+      //   const gesture = await GE.estimate(fingerPose[0].landmarks, 4);
+      //   if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+      //     // console.log(gesture.gestures);
+
+      //     const confidence = gesture.gestures.map(
+      //       (prediction) => prediction.confidence
+      //     );
+      //     const maxConfidence = confidence.indexOf(
+      //       Math.max.apply(null, confidence)
+      //     );
+      //     // console.log(gesture.gestures[maxConfidence].name);
+      //     setEmoji(gesture.gestures[maxConfidence].name);
+      //     console.log(emoji);
+      //   }
+      // }
+
+      /////// NEW STUFF ADDED GESTURE HANDLING
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  };
+
+  useEffect(() => { runHandpose() }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 1050,
+            height: 720,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 1050,
+            height: 720,
+          }}
+        />
+        {/* NEW STUFF */}
+        {emoji !== null ? (
+          <img
+            src={images[emoji]}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 400,
+              bottom: 500,
+              right: 0,
+              textAlign: "center",
+              height: 100,
+            }}
+          />
+        ) : (
+          ""
+        )}
+
+        {/* NEW STUFF */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utilities.js
@@ -0,0 +1,115 @@
+// Points for fingers
+const fingerJoints = {
+  thumb: [0, 1, 2, 3, 4],
+  indexFinger: [0, 5, 6, 7, 8],
+  middleFinger: [0, 9, 10, 11, 12],
+  ringFinger: [0, 13, 14, 15, 16],
+  pinky: [0, 17, 18, 19, 20],
+};
+
+// Infinity Gauntlet Style
+const style = {
+  0: { color: "red", size: 6 },
+  1: { color: "yellow", size: 3 },
+  2: { color: "yellow", size: 3 },
+  3: { color: "yellow", size: 3 },
+  4: { color: "red", size: 6 },
+  5: { color: "yellow", size: 3 },
+  6: { color: "yellow", size: 3 },
+  7: { color: "yellow", size: 3 },
+  8: { color: "red", size: 6 },
+  9: { color: "yellow", size: 3 },
+  10: { color: "yellow", size: 3 },
+  11: { color: "yellow", size: 3 },
+  12: { color: "red", size: 6 },
+  13: { color: "yellow", size: 3 },
+  14: { color: "yellow", size: 3 },
+  15: { color: "yellow", size: 3 },
+  16: { color: "red", size: 6 },
+  17: { color: "yellow", size: 3 },
+  18: { color: "yellow", size: 3 },
+  19: { color: "yellow", size: 3 },
+  20: { color: "red", size: 6 },
+};
+
+// Drawing function
+export const drawHand = (predictions, ctx) => {
+  // Check if we have predictions
+  if (predictions.length > 0) {
+    // Loop through each prediction
+    predictions.forEach((prediction) => {
+      // Grab landmarks
+      // const landmarks = prediction.landmarks;
+      const keypoint = prediction.keypoints;
+
+      // Loop through fingers
+      for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
+        let finger = Object.keys(fingerJoints)[j];
+        //  Loop through pairs of joints
+        for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+          // Get pairs of joints
+          const firstJointIndex = fingerJoints[finger][k];
+          const secondJointIndex = fingerJoints[finger][k + 1];
+
+          // // Draw path
+          // ctx.beginPath();
+          // ctx.moveTo(
+          //   landmarks[firstJointIndex][0],
+          //   landmarks[firstJointIndex][1]
+          // );
+          // ctx.lineTo(
+          //   landmarks[secondJointIndex][0],
+          //   landmarks[secondJointIndex][1]
+          // );
+          // ctx.strokeStyle = "plum";
+          // ctx.lineWidth = 4;
+          // ctx.stroke();
+
+          // console.log(keypoint[firstJointIndex])
+          // Draw path
+          ctx.beginPath();
+          ctx.moveTo(
+            keypoint[firstJointIndex]["x"],
+            keypoint[firstJointIndex]["y"]
+          );
+          ctx.lineTo(
+            keypoint[secondJointIndex]["x"],
+            keypoint[secondJointIndex]["y"]
+          );
+          ctx.strokeStyle = "orange";
+          ctx.lineWidth = 4;
+          ctx.stroke();
+        }
+      }
+
+      // Loop through landmarks and draw em
+      for (let i = 0; i < keypoint.length; i++) {
+        // Get x point
+        const x = keypoint[i]["x"];
+        // Get y point
+        const y = keypoint[i]["y"];
+        // Start drawing
+        ctx.beginPath();
+        ctx.arc(x, y, style[i]["size"], 0, 3 * Math.PI);
+
+        // Set line color
+        ctx.fillStyle = style[i]["color"];
+        ctx.fill();
+      }
+      // Loop through landmarks and draw em
+      for (let i = 0; i < keypoint.length; i++) {
+        // Get x point
+        const x = keypoint[i][0];
+        // Get y point
+        const y = keypoint[i][1];
+        // Start drawing
+        ctx.beginPath();
+        ctx.arc(x, y, style[i]["size"], 0, 3 * Math.PI);
+
+        // Set line color
+        ctx.fillStyle = style[i]["color"];
+        ctx.fill();
+      }
+    });
+  }
+};
